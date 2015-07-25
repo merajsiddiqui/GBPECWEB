974fcd4c2baf72b86586a42a1fc0390dc58ef05a
merajsiddiqui <merajsiddiqui@outlook.com>
2015-07-26 00:32:53 +0530

    Views
    Here are the files stored which are basically the webpage that load on
    the website ...

---
+++ application/views/layouts/errors/500.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Apply to thousands of Internships in Mumbai, bangalore, delhi, chennai, hyderabad for B.Tech, MBA, etc Students" />
+    <meta name="keywords" content="Internship india, Internship in Mumbai, The Internship, Internship training, Internship in bangalore, Summer internship india">
+    <meta name="author" content="https://plus.google.com/107837531266258418226" />
+    <meta name="msvalidate.01" content="B1BCAA5861C00450565B6B8C742070DF" />
+    <meta name="copyright" content="SWIFTDEAL ONLINE LLP 2014">
+    <meta name="email" content="info[at]swiftintern[dot]com">
+    <meta http-equiv="Content-Language" content="en">
+    <meta name="Rating" content="General">
+    <meta name="Distribution" content="Global">
+    <meta name="Robots" content="INDEX,UNFOLLOW">
+    <meta name="Revisit-after" content="1 Day">
+    <link rel="author" href="https://plus.google.com/107837531266258418226" />
+    <link rel="publisher" href="https://plus.google.com/107837531266258418226">
+    <link rel="shortcut icon" href="http://localhost/master/public/public/assets/favicons/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="http://localhost/master/public/public/assets/favicons/favicon.ico" />
+
+    <title>G. B. Pant Engineering College</title>
+        <link href="http://localhost/master/public/public/assets/css/main.css" rel="stylesheet" type="text/css" media="all">
+        <style type="text/css">
+            #osfooter{display:block;position:fixed;bottom:0;left:0;width:100%;height:300px;margin-bottom:-300px;overflow:hidden;background-color:transparent;z-index:5000;text-indent:-5000px;}
+            #osfooter div{margin-bottom:-1000px;}
+            #osfooter a{display:block; text-indent:-5000px;}
+        </style>
+        <!--[if lte IE 6]><style type="text/css">#osfooter{position:absolute; display:none;}</style><![endif]-->
+        <link rel="apple-touch-icon" sizes="57x57" href="http://localhost/master/public/public/assets/img/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="http://localhost/master/public/public/assets/img/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="http://localhost/master/public/public/assets/img/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="http://localhost/master/public/public/assets/img/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="http://localhost/master/public/public/assets/img/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="http://localhost/master/public/public/assets/img/icon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="http://localhost/master/public/public/assets/img/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="http://localhost/master/public/public/assets/img/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="http://localhost/master/public/public/assets/img/icon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="http://localhost/master/public/public/assets/img/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="http://localhost/master/public/public/assets/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="http://localhost/master/public/public/assets/img/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="http://localhost/master/public/public/assets/img/favicon-16x16.png">
+        <link rel="manifest" href="http://localhost/master/public/public/assets/img//manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="http://localhost/master/public/public/assets/img/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="shortcut icon" href="http://localhost/master/public/public/assets/img/favicon.ico" type="image/vnd.microsoft.icon" />
+    </head>
+<body id="top">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]
+
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    #################################################################################################################
+    ###     If you are the kind of person who reads a source code of a website , you may be a great fit for us.   ### 
+    ###     Write to us at info@mmtech.com                                                                   ###
+    #################################################################################################################
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-->
+    
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+    <div class="wrapper row0">
+  <div id="topbar">
+  <div id="slidepanel" class="clear">
+    <div class="fl_left">
+    <form method="post" action="#" class="block clear">
+      <input type="text" name="query" value="" size="35">
+      <button class="button" type="submit" name="search"><span class="icon-search"></span></button>
+    </form>
+  </div>
+  <div class="fl_right">
+    <ul class="social">
+      <li><a class="socico-facebook" title="Facebook" href="https://www.facebook.com/G.B.pant"><span class="icon-facebook"><em>Facebook</em></span></a></li>
+      <li><a class="socico-pinterest" title="Pinterest" href="#"><span class="icon-pinterest"><em>Pinterest</em></span></a></li>
+      <li><a class="socico-twitter" title="Twitter" href="#"><span class="icon-twitter"><em>Twitter</em></span></a></li>
+      <li><a class="socico-dribble" title="Dribble" href="#"><span class="icon-dribbble"><em>Dribble</em></span></a></li>
+      <li><a class="socico-linkedin" title="LinkedIn" href="#"><span class="icon-linkedin"><em>LinkedIn</em></span></a></li>
+      <li><a class="socico-google-plus" title="Google+" href="#"><span class="icon-google-plus"><em>Google+</em></span></a></li>
+      <li><a class="socico-skype" title="Skype" href="#"><span class="icon-skype"><em>Skype</em></span></a></li>
+    </ul>
+  </div>
+  </div>
+  <div id="openpanel">
+    <a id="slideit" class="icon-chevron-down" href="#slidepanel"></a>
+    <a id="closeit" class="icon-chevron-up" style="display:none;" href="#slidepanel"></a>
+  </div>
+  </div>
+</div>
+<div class="wrapper row1">
+  <header role="banner" id="header" class="clear">
+    <img src="http://localhost/master/public/public/assets/img/gbpeclogo.png" align="left" alt="GBPEC logo">
+    <div class="fl_left" style="padding-top:25px">
+      <h1><a>Govind Ballabh Pant Engineering College </a></h1>
+      <h1><p><center  style="font-size:25px;"> Government Of NCT Of Delhi  <br/> Affiliated To Guru Govind Singh Indraprastha University</center>
+      </p></h1>
+    </div>
+    <div class="fl_right right" style="padding-top:25px">
+      <ul class="meta nospace inline">
+        <li><span class="icon-phone"></span> +91 (011) 2638 2314</li>
+        <li><span class="icon-envelope-alt"></span> <a href="mailto:info@gbpec.edu.in">info@gbpec.edu.in</a></li>
+      </ul>
+      <nav>
+        <ul class="nospace inline">
+          <li><a href="">Home</a></li>
+          <li><a href="#">A - Z Index</a></li>
+          <li><a href="http://localhost/GBPECMID/">Memeber Login</a></li>
+        </ul>
+        <ul class="nospace inline" style="padding-top:15px">
+          <li><a href="http://delhi.gov.in/wps/wcm/connect/DOIT_GBPEC/gbpec/home">GBPEC Delhi Govt. Website</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</div>
+<div class="wrapper row2">
+  <nav role="navigation" id="topnav" class="clear">
+    <ul class="clear">
+      <li class=""><a href="">Home</a></li>
+      <li class=""><a class="drop" href="#">About Us</a>
+        <ul class="sub-menu">
+          <li><a href="">About Us</a></li>
+          <li><a href="">Principal Desk</a></li>
+          <li><a class="drop" href="#">Team / Staff</a>
+            <ul class="sub-menu">
+              <li><a href="" >Faculties / Staffs</a></li>
+              <li><a href="">Web Support Team</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class=""><a class="drop" href="#" >Academics</a>
+        <ul class="sub-menu">
+          <li><a class="drop" href="#" >Course Offered</a>
+            <ul class="sub-menu">
+              <li><a href="" >Electronics & Communication Engg.</a></li>
+              <li><a href="">Computer Science Engg.</a></li>
+              <li><a href="" >Mechanical & Automation Engg.</a></li>
+              
+            </ul>
+          </li>
+          <li><a class="drop" href="#" >Laboratry</a>
+            <ul class="sub-menu">
+              <li><a href="" >VLSI LAB</a></li>
+              <li><a href="">Computer Lab</a></li>
+              <li><a href="" >Microwave Lab</a></li>
+              <li><a href="" >DSP Lab</a></li>
+              <li><a href="" >Electronics Lab</a></li>
+              <li><a href="" >Settelite & Antena Lab</a></li>
+              <li><a href="" >Networking Lab</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#" >Social Initiatives</a>
+            <ul class="sub-menu">
+              <li><a href="" >Inceptum</a></li>
+              <li><a href="">Ingenium</a></li>
+              <li><a href="" >GBPEC IEEE</a></li>
+              <li><a href="" >Samidha</a></li>
+              
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Campus Facilities</a>
+            <ul class="sub-menu">
+              <li><a href="">Sports & Games</a></li>
+              <li><a href="">Library</a></li>
+              <li><a href="">Canteen</a></li>
+            </ul>
+          </li>
+          
+          <li><a href="" >FAQ</a></li>
+          <li><a href="">Archives</a></li>
+        </ul>
+      </li>
+      <li><a class="drop" href="#" >Blog</a>
+        <ul class="sub-menu">
+          <li><a href="" >By Faculties</a></li>
+          <li><a href="">Students Pen</a></li>
+        </ul>
+      </li>
+
+      <li class="last-child"><a class="drop" href="#" >Gallery</a>
+        <ul class="sub-menu">
+          <li><a href="">College Infra Structure</a></li>
+          <li><a href="">Fresher's Party</a></li>
+          <li><a href="" >Farewell Party</a></li>
+          <li><a href="">Inceptum</a></li>
+          <li><a href="">Other Events</a></li>
+        </ul>
+      </li>
+      <li ><a href="" >Student Activities</a></li>
+      <li ><a href="" >GBPEC Bulletin</a></li>
+      <li ><a href="" >Alumni</a></li>
+      <li class=""><a href="" >Contact Us</a></li>
+    </ul>
+  </nav>
+</div>
+
+<div class="wrapper row4">
+  <div role="main" class="container">
+    <div id="fof" class="clear">
+      <h1>500</h1>
+      <h2>An error occured</h2>
+      <p class="notice">SORRY ! We are struggling coding here<br>
+        Some Internal Server Error Occured</p>
+      <p>Return to the <a href="#">Homepage</a> or use the search form below to find what you are looking for:</p>
+      <form method="post" action="#">
+        <input type="text" value="">
+        <button class="button red" type="submit"><span class="icon-search"></span></button>
+      </form>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+   
+<div class="wrapper row6">
+  <footer role="contentinfo" id="p-footer" class="clear center">
+    <div class="col-1-5 first">
+      <div class="block push30"><span class="cc circle icon-map-marker"></span></div>
+      <h6 class="push20">Address</h6>
+      <address>
+      GovindPuri, Okhla Phase-III ,<br>
+      New Delhi, 110020
+      </address>
+    </div>
+    <div class="col-1-5">
+      <div class="block push30"><span class="cc circle icon-phone"></span></div>
+      <h6 class="push20">Phone</h6>
+      <p class="nospace">Phone : +91-(011)-2638 2314<br>
+        Fax :  +91-(011)-2638 2317</p>
+    </div>
+    <div class="col-1-5">
+      <div class="block push30"><span class="cc circle icon-envelope-alt"></span></div>
+      <h6 class="push20">Email</h6>
+      <p class="nospace">
+        <a href="#">info@gbpec.edu.in</a><br>
+        <a href="#">support@gbpec.edu.in</a>
+      </p>
+    </div>
+    <div class="col-2-5">
+      <div class="block push30"><span class="cc circle icon-file-text-alt"></span></div>
+      <h6 class="push20">Newsletter</h6>
+      <form method="post" action="#">
+        <input type="text" value="">
+        <input type="submit" value="Subscribe">
+      </form>
+    </div>
+  </footer>
+  <div id="copyright" class="clear">
+    <div class="fl_left">Copyright &copy; 2007 - 2015 <a href="">  G. B. Pant Engineering College</a>. All Rights Reserved , ISO : 9001-2008 Cerified</div>
+    <div class="fl_right">
+      <ul class="nospace inline">
+        <li><a href="#">Privacy Policy</a> / </li>
+        <li><a href="#">Terms Of Use</a> / </li>
+        <li><a href="#">FAQ</a> / </li>
+        <li><a href="#">Sitemap</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+    
+    
+<a href="#top" id="scrolltotop" title="Back To Top"><span class="icon-arrow-up icon-2x"></span></a> 
+<script src="http://localhost/master/public/public/assets/js/jquery-latest.min.js"></script>
+<script src="http://localhost/master/public/public/assets/js/jquery-ui.min.js"></script>
+<script src="http://localhost/master/public/public/assets/js/jquery.fitvids.js"></script>
+<script src="http://localhost/master/public/public/assets/js/jquery.flexslider-min.js"></script>
+<script src="http://localhost/master/public/public/assets/js/jquery.flexslider-setup.js"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script src="http://localhost/master/public/public/assets/js/setup.js"></script>
+<script src="http://localhost/master/public/public/assets/js/nivo-lightbox.min.js"></script>
+<script src="http://localhost/master/public/public/assets/js/nivo-lightbox-setup.js"></script>
+<script src="http://localhost/master/public/public/assets/js/custom.js"></script>
+</body>
+</html>
